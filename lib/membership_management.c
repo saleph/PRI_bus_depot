bac2f6b63f6a8ffced1de7ef6670c7f2291c1a20
Tomasz Galecki <tomasz.galecki@hotmail.com>
2015-12-02 11:44:25 +0100

    [ADD] move assignment

--- lib/membership_management.c
+++ lib/membership_management.c
@@ -30,3 +30,15 @@ void remove_assignment_from(char* depot_name, int side_no)
     remove_from(&(the_depot->members), the_bus);
     remove_from(&(the_bus->memberships), the_depot);
 }
+
+void move_to(char *depot_name, int side_no)
+{
+    Bus *the_bus;
+    Depot *the_depot;
+    the_bus = find_object_with_item_in(&buses, (void*)(&side_no), get_side_no, side_no_cmp);
+    the_depot = find_object_with_item_in(&depots, (void*)depot_name, get_depot_name, depots_names_cmp);
+    if (!the_bus || !the_depot) {
+        msg(NOT_FOUND);
+        return;
+    }
+}
