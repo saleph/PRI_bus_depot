129bc968d06238a95c96329ea09d4778712dd51a
Tomasz Galecki <tomasz.galecki@hotmail.com>
2015-12-05 00:16:46 +0100

    [ADD] saving data into text file

--- lib/dumper.c
+++ lib/dumper.c
@@ -2,36 +2,78 @@
 
 
 static FILE *file_hook;
-static void dump_comments();
 static void dump_bus(void*);
 static void dump_depot(void*);
+static void dump_bus_as_ref(void*);
+
+static void start_comments();
+static void buses_now_comment();
+static void depots_now_comment();
 
 
 int dump_data_to(const char* filename)
 {
-    file_hook = fopen(filename, 'w');
-    if (file_hook) {
+    file_hook = fopen(filename, "w");
+    if (!file_hook) {
         msg(FILE_OPEN_FAILED);
         return 0;
     }
-    dump_comments();
+    start_comments();
+
+    buses_now_comment();
     do_for_each_in(&buses, dump_bus);
+
+    depots_now_comment();
     do_for_each_in(&depots, dump_depot);
+
     fclose(file_hook);
     return 1;
 }
 
-void dump_comments()
+void buses_now_comment()
 {
-    fprintf(file_hook, "# This is the comment line. Comments will be ignored during parsing\n");
-    fprintf(file_hook, "# Record with a BUS starts with 'B' letter\n");
-    fprintf(file_hook, "# Record with a DEPOT starts with 'D' letter\n");
-    fprintf(file_hook, "# Each field in each record has to be separated by ';' (semicolon)\n");
+    fprintf(file_hook, "# ================== Buses ==================\n");
+    fprintf(file_hook, "# b: <side no>, <line no>, <driver name>, <driver pesel>;\n");
+    fprintf(file_hook, "\n");
 }
 
 void dump_bus(void* the_bus_ptr)
 {
     Bus *the_bus;
     the_bus = (Bus*)the_bus_ptr;
+    fprintf(file_hook, "b: %d, %d, %s, %s;\n", the_bus->side_no,
+            the_bus->line_no, the_bus->driver_name, the_bus->driver_pesel);
+}
 
+void depots_now_comment()
+{
+    fprintf(file_hook, "\n");
+    fprintf(file_hook, "# ================== Depots =================\n");
+    fprintf(file_hook, "# d: <depot name>, <side no of bus 1>,..., <side no of bus n>;\n");
+    fprintf(file_hook, "\n");
+}
+
+void dump_depot(void* the_depot_ptr)
+{
+    Depot *the_depot;
+    the_depot = (Depot*)the_depot_ptr;
+    fprintf(file_hook, "d: %s", the_depot->name);
+    do_for_each_in(&(the_depot->members), dump_bus_as_ref);
+    fprintf(file_hook, ";\n");
+}
+
+void dump_bus_as_ref(void* the_bus_ptr)
+{
+    Bus *the_bus;
+    the_bus = (Bus*)the_bus_ptr;
+    fprintf(file_hook, ", %d", the_bus->side_no);
+}
+
+void start_comments()
+{
+    fprintf(file_hook, "# This is the comment line. Comments will be ignored during parsing\n");
+    fprintf(file_hook, "# Record with a BUS starts with 'b:'\n");
+    fprintf(file_hook, "# Record with a DEPOT starts with 'd:'\n");
+    fprintf(file_hook, "# Fields in each record has to be separated by ',' (comma) and ends with ';' (semicolon)\n");
+    fprintf(file_hook, "\n\n");
 }
--- main.c
+++ main.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 #include "include/tests.h"
 #include "include/cleanup.h"
+#include "include/dumper.h"
 
 
 int main()
 {
     create_data();
+    dump_data_to("data.txt");
     print_data();
     change_some_references();
     printf("\n\n");
